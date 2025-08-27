82fdc78bef2f0974c87a2738d7bfff9dd5d49c79
Andy Yang <2878847069@qq.com>
2025-08-27 23:13:38 +0800

    feat: add custom env

--- config/utils/index.js
+++ config/utils/index.js
@@ -1,5 +1,8 @@
 import { resolve } from 'node:path'
 import fs from 'node:fs'
+import chalk from 'chalk'
+
+const log = (str) => console.log(str)
 
 // 动态读取 pages 目录下包含 index.html 的子目录
 export const getContextPages = (pagesContext) => {
@@ -25,4 +28,26 @@ export const getContextPages = (pagesContext) => {
   }
 
   return pages
+}
+
+export const getEntryPage = (pagesContext, pageName) => {
+	const pages = getContextPages(pagesContext)
+  if (!pageName) {
+    log(
+      chalk.red('Command line might be wrong. Usage: pnpm dev -- --page=pageName')
+    )
+    process.exit(1)
+  } else {
+    log(chalk.yellow(`Building page: ${pageName}`))
+  }
+  const filterArr = pages.filter(
+    (item) => item.page.toLowerCase() === pageName.toLowerCase()
+  )
+  if (!filterArr.length) {
+    log(chalk.red(`Page '${pageName}' not found. Available pages: ${pages.map(p => p.page).join(', ')}`))
+		process.exit(1)
+  }
+  return {
+    [pageName]: resolve(__dirname, `${pagesContext}/${pageName}/index.html`)
+  }
 }
--- src/pages/client/main.js
+++ src/pages/client/main.js
@@ -1,1 +1,2 @@
 console.log('Client')
+console.log(import.meta.env)
